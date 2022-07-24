82cc7bdfbabbab034c81dcea67741d3e7c9500c8
Jein Song <jein@scatterlab.co.kr>
2022-07-24 06:38:48 +0000

    refactor: 전처리 코드 정리
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,7 @@ from glob import glob
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit
 from tqdm import tqdm
 
 
@@ -21,29 +22,33 @@ if __name__ == "__main__":
         read_notebook(path) for path in tqdm(train_paths, desc="Read Train Notebooks")
     ]
 
-    df_train = (
+    #: 노트북 아이디, 셀 아이디, 셀 타입, 소스(텍스트)
+    df_all = (
         pd.concat(train_notebooks)
         .set_index("id", append=True)
         .swaplevel()
         .sort_index(level="id", sort_remaining=False)
     )
-    df_orders = pd.read_csv(
-        "./data/train_orders.csv", index_col="id", squeeze=True
-    ).str.split()
 
+    #: 노트북 아이디별 cell_order(셀 순서)
+    df_orders = pd.read_csv(
+        "./data/train_orders.csv", index_col="id", squeeze=True  # Series로 리턴 됨
+    ).str.split()  # string 표현을 리스트로 스플릿
+
+    #: 노트북 아이디별 cell_order(셀 정답 순서), cell_id(주어진 셀 아이디 순서)
     merged_df_orders = df_orders.to_frame().join(
-        df_train.reset_index("cell_id").groupby("id")["cell_id"].apply(list),
+        df_all.reset_index("cell_id").groupby("id")["cell_id"].apply(list),
         how="right",
     )
 
-    def get_ranks(base, derived):
-        return [base.index(d) for d in derived]
+    def get_ranks(gt, derived):
+        return [gt.index(d) for d in derived]
 
     ranks = {}
     for id_, cell_order, cell_id in merged_df_orders.itertuples():
         ranks[id_] = {"cell_id": cell_id, "rank": get_ranks(cell_order, cell_id)}
 
-    # 주어진 cell id에 원래 순서를 매핑한 df를 만듦
+    #: 주어진 cell id에 원래 순서를 매핑한 df를 만듦
     df_ranks = (
         pd.DataFrame.from_dict(ranks, orient="index")
         .rename_axis("id")
@@ -52,24 +57,22 @@ if __name__ == "__main__":
     )
 
     df_ancestors = pd.read_csv("./data/train_ancestors.csv", index_col="id")
-    df_train = (
-        df_train.reset_index()
+    df_all = (
+        df_all.reset_index()
         .merge(df_ranks, on=["id", "cell_id"])
         .merge(df_ancestors, on=["id"])
     )
-    df_train["pct_rank"] = df_train["rank"] / df_train.groupby("id")[
-        "cell_id"
-    ].transform("count")
-    df_train.to_csv("./data/train.csv")
+    df_all["pct_rank"] = df_all["rank"] / df_all.groupby("id")["cell_id"].transform(
+        "count"
+    )
 
     # validation split
-    df = df_train
     valid_split = 0.1
 
     splitter = GroupShuffleSplit(n_splits=1, test_size=valid_split, random_state=42)
-    train_ind, val_ind = next(splitter.split(df, groups=df["ancestor_id"]))
-    df_train = df.loc[train_ind].reset_index(drop=True)
-    df_valid = df.loc[val_ind].reset_index(drop=True)
+    train_ind, val_ind = next(splitter.split(df_all, groups=df_all["ancestor_id"]))
+    df_train = df_all.loc[train_ind].reset_index(drop=True)
+    df_valid = df_all.loc[val_ind].reset_index(drop=True)
 
     df_train_md = df_train[df_train["cell_type"] == "markdown"].reset_index(drop=True)
     df_valid_md = df_valid[df_valid["cell_type"] == "markdown"].reset_index(drop=True)
@@ -98,6 +101,10 @@ if __name__ == "__main__":
             return results
 
     def get_features(df):
+        """
+        .. todo::
+            더 똑똑하게 추출하기
+        """
         features = dict()
         df = df.sort_values("rank").reset_index(drop=True)
         for idx, sub_df in tqdm(df.groupby("id")):
